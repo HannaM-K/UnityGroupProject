628b187b9ca0247aeaa4e726d9b27ce789e70f09
Adarat <adarat132@gmail.com>
2019-11-05 22:57:11 +0100

    Poprawa

--- UnityGroupProject/Assets/Scripts/Enemy1Movement.cs
+++ UnityGroupProject/Assets/Scripts/Enemy1Movement.cs
@@ -8,18 +8,19 @@ public class Enemy1Movement : MonoBehaviour
     public float speed = 3;
     public bool moveLeft = true;
     public Transform wykrywacz;
-    Vector2 kierunek = new Vector2(1, 0);
 
 
-// Update is called once per frame
+  
+
+    // Update is called once per frame
+
     public void FixedUpdate()
       {
         
         RaycastHit2D Podloga = Physics2D.Raycast(wykrywacz.position, Vector2.down, 1);
-        RaycastHit2D WykryjGracza = Physics2D.Raycast(wykrywacz.position, kierunek, 3);
+       
 
 
-        
 
 
         if (Podloga.collider == true)
@@ -36,13 +37,13 @@ public class Enemy1Movement : MonoBehaviour
             {
                 transform.eulerAngles = new Vector3(0, -180, 0);
                 moveLeft = false;
-                kierunek = new Vector2(-1, 0);
+
             }
             else
             {
                 transform.eulerAngles = new Vector3(0, 0, 0);
                 moveLeft = true;
-                kierunek = new Vector2(1, 0);
+
             }
 
 
