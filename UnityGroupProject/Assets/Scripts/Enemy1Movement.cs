1a2c4b58416484ee200fa79a0b3c2abc03f28be2
Adarat <adarat132@gmail.com>
2019-11-05 22:02:49 +0100

    Przeciwnik

---
+++ UnityGroupProject/Assets/Scripts/Enemy1Movement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1Movement : MonoBehaviour
+{ 
+
+    public float speed = 3;
+    public bool moveLeft = true;
+    public Transform wykrywacz;
+    Vector2 kierunek = new Vector2(1, 0);
+
+
+// Update is called once per frame
+    public void FixedUpdate()
+      {
+        
+        RaycastHit2D Podloga = Physics2D.Raycast(wykrywacz.position, Vector2.down, 1);
+        RaycastHit2D WykryjGracza = Physics2D.Raycast(wykrywacz.position, kierunek, 3);
+
+
+        
+
+
+        if (Podloga.collider == true)
+        {
+            if (!Podloga.collider.CompareTag("Player"))
+                {
+                transform.Translate(Vector2.left * speed * Time.deltaTime);
+            }
+
+        }
+        else 
+        {
+            if (moveLeft == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                moveLeft = false;
+                kierunek = new Vector2(-1, 0);
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                moveLeft = true;
+                kierunek = new Vector2(1, 0);
+            }
+
+
+        }
+
+
+
+    }
+
+
+
+}
