f727240bb84e4856995ca10953d3bb2dd9fe716b
Adarat <adarat132@gmail.com>
2019-11-06 19:23:57 +0100

    Player and enemy death + lifes counter

--- UnityGroupProject/Assets/Scripts/Enemy1Movement.cs
+++ UnityGroupProject/Assets/Scripts/Enemy1Movement.cs
@@ -1,16 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy1Movement : MonoBehaviour
-{ 
-
+{
+    public int lifes = 3;
+    public Text LifeCounter;
     public float speed = 3;
     public float run = 1;
     public bool moveLeft = true;
     public Transform detector;
     public Transform eyes;
     public Vector2 kierunek = new Vector2(-1, 0);
+
+
+    void Awake()
+    {
+        LifeCounter.text = "Ilość żyć: "+lifes;
+    }
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.contacts.Length > 0)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                ContactPoint2D contact = collision.contacts[0];
+                if (Vector2.Dot(contact.normal, Vector2.down) > 0.5)
+                {
+                    Destroy(gameObject);
+                }
+                else
+                {
+                    lifes = lifes - 1;
+                    LifeCounter.text = "Ilość żyć: " + lifes;
+                    Debug.Log("Tu giniesz. WOW");
+                }
+            }
+            else
+            {
+                ContactPoint2D contact = collision.contacts[0];
+                if (Vector2.Dot(contact.normal, Vector2.left) > 0.5)
+                {
+                    transform.eulerAngles = new Vector2(0, -180);
+                    moveLeft = false;
+                }
+                else if (Vector2.Dot(contact.normal, Vector2.right) > 0.5)
+                {
+                    transform.eulerAngles = new Vector2(0, 0);
+                    moveLeft = true;
+                }
+            }
+
+        }
+    }
+    
+
+   
 
     public void FixedUpdate()
       {
@@ -42,13 +90,13 @@ public class Enemy1Movement : MonoBehaviour
         {
             if (moveLeft == true)
             {
-                transform.eulerAngles = new Vector3(0, -180, 0);
+                transform.eulerAngles = new Vector2(0, -180);
                 moveLeft = false;
                 kierunek = new Vector2(1, 0);
             }
             else
             {
-                transform.eulerAngles = new Vector3(0, 0, 0);
+                transform.eulerAngles = new Vector2(0, 0);
                 moveLeft = true;
                 kierunek = new Vector2(-1, 0);
             }
--- UnityGroupProject/Assets/Scripts/EnemyDeath.cs
+++ UnityGroupProject/Assets/Scripts/EnemyDeath.cs
@@ -5,13 +5,33 @@ using UnityEngine;
 public class EnemyDeath : MonoBehaviour
 {
 
-    void OnTriggerEnter2D(Collider2D other)
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.contacts.Length > 0)
+        {
+            if(collision.gameObject.tag == "Player"){
+                ContactPoint2D contact = collision.contacts[0];
+                if (Vector2.Dot(contact.normal, Vector2.down) > 0.5)
+                {
+                    Destroy(gameObject);
+                }
+                else
+                {
+                    Debug.Log("Tu giniesz. WOW");
+                }
+            }
+
+        }
+    }
+    /*
+    void OnCollision2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
             Destroy(transform.parent.gameObject);
         }
     }
+    */
     // Start is called before the first frame update
     void Start()
     {
