1b5de287b5e22999bdc9c6df9adc213934ea047c
Kladek00 <moste6@gmail.com>
2019-11-13 23:26:16 +0100

    Added coins and score releted features
    Added a prefab with coin placeholder. Touchign coin prefabs will correctly add 1 to coin score.
    Added score releted features. Killing an enemy will now add 100 points to players score.
    Both scoreboards are displayed in top left corner.

---
+++ UnityGroupProject/Assets/Scripts/coinScoreScr.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class coinScoreScr : MonoBehaviour
+{
+    Text coinText;
+    public static int coinAmount;
+
+    void Start()
+    {
+        coinText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        coinText.text = "Coins: " + coinAmount.ToString();
+    }
+}
---
+++ UnityGroupProject/Assets/Scripts/scoreScr.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scoreScr : MonoBehaviour
+{
+    public static int scoreVal = 0;
+    Text score;
+
+    void Start()
+    {
+        score = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = "Score: " + scoreVal;
+    }
+}
