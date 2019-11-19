03aee7389b8b89af8f29775317f13553dc41fdce
EvilWitch <osiakot7211@gmail.com>
2019-11-19 20:57:22 +0100

    added map
    added map with collision (temporary grafics) and placeholder sprite for checkpoint

---
+++ UnityGroupProject/Assets/Prefabs/map/Game Objects Tiles.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="Game Objects Tiles" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="Pixel Platformer Art Pack/Tilesets/Game Objects Tiles.png" trans="ff00ff" width="128" height="128"/>
+ <tile id="12">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="10" width="32" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="10" width="32" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="10" width="32" height="12"/>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ UnityGroupProject/Assets/Prefabs/map/Ground Tiles.tsx
@@ -0,0 +1,105 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="Ground Tiles" tilewidth="32" tileheight="32" tilecount="25" columns="5">
+ <image source="Pixel Platformer Art Pack/Tilesets/Ground Tiles.png" trans="ff00ff" width="160" height="160"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="4" x="0.0909091" y="0.0909091" width="31.9091" height="28.9091"/>
+   <object id="5" x="4" y="0" width="28" height="29"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="29"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="28" height="29"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index">
+   <object id="1" x="2" y="0" width="28" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index">
+   <object id="1" x="4" y="0" width="28" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="28" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index">
+   <object id="1" x="3" y="0" width="29" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="29" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index">
+   <object id="1" x="3" y="0" width="29" height="29"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="29"/>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="29" height="29"/>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="19">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="20">
+  <objectgroup draworder="index">
+   <object id="2" x="3" y="3" width="29" height="29"/>
+  </objectgroup>
+ </tile>
+ <tile id="21">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="3" width="32" height="29"/>
+  </objectgroup>
+ </tile>
+ <tile id="22">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="3" width="29" height="29"/>
+  </objectgroup>
+ </tile>
+ <tile id="23">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="24">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+</tileset>
